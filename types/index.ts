f97eb82c5c7a33c7996cdb0a07337d5c4d7c2fbc
aadv1k <aadv1k@outlook.com>
2023-05-31 00:06:58 +0530

    update the types

--- types/index.ts
+++ types/index.ts
@@ -2,13 +2,20 @@ export type Token = FilterToken | WordToken;
 
 export interface GenericFilter {
   type: string,
-  data: Array<string>
+  data: Array<string | Date>
+}
+
+export interface GenericSorter {
+    type: string,
+    condition?: string,
+    by?: string
 }
 
 export interface SearchIntent {
   summary: string,
   keywords: Array<string>,
-  filters: Array<GenericFilter>
+  filters: Array<GenericFilter>,
+  sorters: Array<GenericSorter>
 }
 
 export enum TokenType {
