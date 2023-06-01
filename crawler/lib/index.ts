6e1c28d9f139644e2c4d2f55ac0cbb949b73a69a
aadv1k <aadv1k@outlook.com>
2023-06-01 17:15:06 +0530

    remove several redundant files in the scraper

--- crawler/lib/index.ts
+++ crawler/lib/index.ts
@@ -1,74 +1,16 @@
-import puppeteer from "puppeteer";
+import puppeteer, { Page, Puppeteer } from "puppeteer";
 import { EventEmitter } from "node:events";
 
 import dork from "@byod/dork";
-import { SearchIntent } from "@byod/types";
+import layer1Resolver from "./layer1"
 
-interface CrawlerStatus {
-  status: "progress" | "success",
-  message: string,
-  data?: any
+
+async function crawl(query: string): Promise<void> {
+  let intent = dork(query);
+  console.log(`[INFO] searching: ${intent.summary}`);
+  const layer1 = new layer1Resolver(intent);
+  const data = await layer1.fetchKaggle();
+  console.log(data);
 }
 
-function emitEvent(context: EventEmitter, obj: CrawlerStatus) {
-  context.emit(obj.status, obj);
-}
-
-class Crawler {
-  query: SearchIntent;
-
-  constructor(query: string) {
-    this.query = dork(query);
-  }
-
-  async crawl(): Promise<void> {
-    const kaggleQuery = encodeURIComponent(this.query.keywords.join(" "));
-    const kaggleFilters = encodeURIComponent("datasetFileTypes:csv") + "+" +
-                      encodeURIComponent("datasetLicense:Commercial")
-    const kaggleSearch = `https://www.kaggle.com/search?q=${kaggleQuery}+${kaggleFilters}` 
-    
-    /*
-    emitEvent(this, {
-      status: "progress",
-      message: `searching on kaggle: ${kaggleSearch}`
-    })
-    */
-    const browser = await puppeteer.launch({ headless: "new" });
-    const page = await browser.newPage();
-
-    await page.setJavaScriptEnabled(true);
-    await page.goto(kaggleSearch);
-
-    setTimeout(async () => {
-      await page.waitForSelector(".sc-dQDPHY");
-      const ulElement = await page.$(".sc-dQDPHY");
-      const pageLinks = (await ulElement?.$$("a"))?.map(e => {
-          return e.evaluate((i) =>
-            i.getAttribute("href")
-          );
-      })
-      const links = await Promise.all(pageLinks as any);
-
-      const pageMeta = (await ulElement?.$$("li"))?.map((e, idx) => {
-          return e.evaluate((i) => {
-            return {
-              title: i.getElementsByTagName("h6")[0].textContent,
-              published: i.querySelector("div:nth-child(1) > div:nth-child(2) > div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1)")?.textContent,
-            }
-          }); 
-        })
-      let meta = await Promise.all(pageMeta as any);
-      meta = meta.map((e: any, i) => {
-        return {
-          ...e,
-          link: "https://kaggle.com" + links[i],
-        };
-      });
-      console.log(meta);
-      await browser.close();
-    }, 3_000);
-  }
-}
-
-const crawler = new Crawler("bitcoin pricing data 2023");
-crawler.crawl();
+crawl("bitcoin pricing data");
--- crawler/lib/layer1/index.ts
+++ crawler/lib/layer1/index.ts
@@ -1,2 +1,82 @@
+import puppeteer, { Page, Puppeteer } from "puppeteer";
+import { EventEmitter } from "node:events";
 
+import { SearchIntent } from "@byod/types"
 
+import { CACHE_PATH } from "../const";
+
+import path from "node:path";
+import fs from "node:fs";
+
+interface CrawlerStatus {
+  status: "progress" | "success",
+  message: string,
+  data?: any
+}
+
+interface CrawlerIndex {
+  title: string,
+  keywords: Array<string>,
+  download: string
+}
+
+function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export default class Crawler extends EventEmitter {
+  query: SearchIntent;
+  cacheDir: string;
+
+  constructor(query: SearchIntent) {
+    super();
+    this.query = query;
+    this.cacheDir = path.join(__dirname, CACHE_PATH);
+  }
+
+  async fetchKaggle(): Promise<Array<CrawlerIndex>> {
+    const kaggleQuery = encodeURIComponent(this.query.keywords.join(" "));
+    const kaggleFilters = encodeURIComponent("datasetFileTypes:csv") + "+" +
+                      encodeURIComponent("datasetLicense:Commercial")
+    const kaggleSearch = `https://www.kaggle.com/search?q=${kaggleQuery}+${kaggleFilters}` 
+
+    const kaggleTarget = ".sc-dQDPHY"
+    
+    const browser = await puppeteer.launch({ headless: "new" });
+    const page = await browser.newPage();
+
+    await page.setJavaScriptEnabled(true);
+    await page.goto(kaggleSearch);
+    await sleep(1_500);
+
+    await page.waitForSelector(kaggleTarget);
+    const ulElement = await page.$(kaggleTarget);
+
+    const pageLinks = (await ulElement?.$$("a"))?.map(e => {
+        return e.evaluate((i) => i.getAttribute("href"));
+    })
+    const links = await Promise.all(pageLinks as any);
+
+    const pageTitles = (await ulElement?.$$("li"))?.map((e, idx) => {
+           return e.evaluate((i) => i.getElementsByTagName("h6")[0].textContent); 
+    })
+    let titles = await Promise.all(pageTitles as any);
+
+    await browser.close();
+
+    return links.map((link, idx) => {
+          return {
+              title: titles[idx],
+              keywords: titles[idx].trim().split(" "),
+              download: `https://kaggle.com${link}/download`,
+          }
+      })
+  }
+
+  index(filepath?: string): void {
+     if (!fs.existsSync(this.cacheDir)) fs.mkdirSync(this.cacheDir)
+     filepath = filepath ?? "./byod-layer1.json"
+     const cachePath = path.join(this.cacheDir, filepath);
+  }
+
+}
