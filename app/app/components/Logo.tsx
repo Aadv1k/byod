bc61699cef8aad8547d813a26584e75a3a82b398
aadv1k <aadv1k@outlook.com>
2023-06-08 13:23:39 +0530

    remove the app implementation for now

--- app/app/components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@mui/material";
-
-export default function Logo ({variant}: {variant: string}) {
-  return (
-            <Typography textAlign="center" variant={variant} component="h1" fontWeight={800} color="grey.800">
-                BuildYourOwnData
-            </Typography>
-  )
-}
--- app/app/components/MuiTheme.ts
+++
@@ -1,7 +0,0 @@
-import { createTheme } from '@mui/material/styles';
-
-export default createTheme({
-    typography: {
-        fontFamily: "Inter, sans-serif"
-    }
-})
